5c2d670ac247bd62e72c40ac13628396507c2477
Osfran Murillo <botsito051981@gmail.com>
2026-09-23 17:46:46 -0600

    feat: Hoy Avance con mi plan justamente dia5-semana1 de ts.

---
+++ src/dia5-semana1.ts
@@ -0,0 +1,26 @@
+// funcion que verifica acceso. Se ingresa su Id puede ser string o number, y si el usuario no verifica si es Administrador se pone valor por defecto.
+function verificarNivelAcceso(id: string | number, esAdmin: boolean = false): string {
+    return `Acceso concedido para el usuario ${id} (Admin: ${esAdmin ? true : false})`;
+};
+
+console.log(verificarNivelAcceso(7832784533, true));
+console.log(verificarNivelAcceso(7832784533));
+
+// funcion similar a la que realize el dia3-semna1 que consiste en pasar una lista de notas de 4 parciales y luego te devuelv tu promedio.
+function calcularPromedioCalificaciones(calificacion: number[]): number  {
+    return (calificacion[0] + calificacion[1] + calificacion[2] + calificacion[3]) / 4;
+};
+
+console.log(calcularPromedioCalificaciones([89,93,89,79]));
+
+// funcion que muestra un tipo de error de sistema.
+function mostrarAlertaSistema(mensaje: string, codigo?: number): void {
+    if (codigo !== undefined) {
+        console.log(`${mensaje} su alerta de sistemas. Su codigo: ${codigo}`);
+    } else {
+        console.log(`${mensaje} su alerta de sistemas.`);
+    }
+};
+
+mostrarAlertaSistema('Mensaje');
+mostrarAlertaSistema('Mensaje', 401);
